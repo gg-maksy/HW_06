e2d68f70d2d4164b0c968261067496fbb6941399
gg-maksy <maksimusoid98@gmail.com>
2023-03-08 03:05:34 +0200

    rewrite the code. this app are working badly now

--- sort.py
+++ sort.py
@@ -1,78 +1,52 @@
 import sys
+import re
 from pathlib import Path
-import shutil
 from dictionary import transliterate
 
+FORMATS = {}
+folders_to_del = []
 
 try:
     path = Path(sys.argv[1])
 except IndexError:
     print('Sorry. No param.')
 
-create = None
 
-def get_folders(path: Path):
-
-    global create
-    create = Path(f'{path}\\all_files')
-    create.mkdir(exist_ok=True)
-
-    def open_folder(name: Path):
-
-        for item in name.iterdir():
-
-            if not item.is_dir():
-                shutil.move(item, create)
-            if item.is_dir():
-                open_folder(item)
-    
-    open_folder(path)
+def get_files(path: Path, file: Path):
+    file.replace(path / item.name)
 
 
-def remove_empty_folders(path: Path):
-    count_ch = 0
-    for i in path.iterdir():
-        if i != create and i.is_dir():
-            try:
-                i.rmdir()
-            except OSError:
-                remove_empty_folders(i)
+for item in path.glob('**/*.*'):
+    get_files(path, item)
 
-    for ch in path.iterdir():
-        count_ch += 1
+for item in path.glob('**'):
+    folders_to_del.append(item)
 
-    if count_ch > 1:
-        remove_empty_folders(path)
+for item in folders_to_del[::-1]:
+    try:
+        item.rmdir()
+    except Exception as e:
+        print(e)
 
-        
+with open('format.txt', 'r') as fd:
+    line = fd.readline()
+    while line:
 
-get_folders(path)
+        k = re.search(r'^\w+', line)
+        v = re.findall(r'\.\w+', line)
+        FORMATS.update({k.group() : v})
+        line = fd.readline()
 
-remove_empty_folders(path)
+for file in path.iterdir():
+    for k, v in FORMATS.items():
+        for i in v:
+            if file.suffix in i:
+                fold_i = Path(f'{path}\{k}')
+                fold_i.mkdir(exist_ok=True)
+                file.replace(fold_i / file.name)
+            else:
+                fold_a = Path(f'{path}\\another')
+                fold_a.mkdir(exist_ok=True)
+                file.replace(fold_a / file.name)
 
-for item in create.iterdir():
-    if item.suffix[1:] in ('jpeg', 'png', 'jpg', 'svg'):
-        fold_i = Path(f'{path}\\images')
-        fold_i.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_i)
-    elif item.suffix[1:] in ('avi', 'mp4', 'mov', 'mkv'):
-        fold_v = Path(f'{path}\\videos')
-        fold_v.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_v)
-    elif item.suffix[1:] in ('doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx', 'rtf'):
-        fold_d = Path(f'{path}\\docs')
-        fold_d.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_d)
-    elif item.suffix[1:] in ('mp3', 'ogg', 'wav', 'amr'):
-        fold_m = Path(f'{path}\\musics')
-        fold_m.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_m)
-    elif item.suffix[1:] in ('zip', 'gz', 'tar', 'rar'):
-        fold_a = Path(f'{path}\\archives')
-        fold_a.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_a)
-    else:
-        fold_anot = Path(f'{path}\\another')
-        fold_anot.mkdir(exist_ok=True)
-        shutil.move(item.replace(transliterate(item.name)), fold_anot)
-create.rmdir()
+
